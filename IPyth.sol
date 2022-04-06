3edf5ac204b2c5df89ac75b9eaac01947095e38b
Tom Pointon <tom@teepeestudios.net>
2022-04-06 10:55:53 +0000

    Add Pyth SDK structs and interface

---
+++ IPyth.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.0;
+
+import "./PythStructs.sol";
+
+interface Pyth {
+    
+    // Returns the latest price information for the given price ID
+    function queryPriceFeed(bytes32 id) public view returns (PythStructs.PriceFeedResponse memory priceFeed)
+
+}
