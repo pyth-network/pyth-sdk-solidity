8499c6120a648eb72a542f92895fbe3a5564bf34
Tom Pointon <tom@teepeestudios.net>
2022-04-11 09:45:46 +0000

    Add IPyth interface

--- IPyth.sol
+++ IPyth.sol
@@ -3,9 +3,26 @@ pragma solidity ^0.8.0;
 
 import "./PythStructs.sol";
 
+/// @title Consume prices from the Pyth Network (https://pyth.network/).
+/// @author Pyth Data Association
 interface IPyth {
-    
-    // Returns the latest price information for the given price ID
-    function queryPriceFeed(bytes32 id) public view returns (PythStructs.PriceFeedResponse memory priceFeed)
+
+    /// @notice Returns the current price and confidence interval.
+    /// @dev Reverts if the current price is not available.
+    /// @param id The Pyth Price Feed ID of which to fetch the current price and confidence interval.
+    /// @return price - please read the documentation of PythStructs.Price to understand how to use this safely.
+    function getCurrentPrice(bytes32 id) external returns (PythStructs.Price memory price);
+
+    /// @notice Returns the exponential moving average price and confidence interval.
+    /// @dev Reverts if the current exponential moving average price is not available.
+    /// @param id The Pyth Price Feed ID of which to fetch the current price and confidence interval.
+    /// @return price - please read the documentation of PythStructs.Price to understand how to use this safely.
+    function getEMAPrice(bytes32 id) external returns (PythStructs.Price memory price);
+
+    /// @notice Returns the most recent previous price with a status of Trading, with the time when this was published.
+    /// @dev This may be a price from arbitrarily far in the past: it is important that you check the publish time before using the price.
+    /// @return price - please read the documentation of PythStructs.Price to understand how to use this safely.
+    /// @return publishTime - the UNIX timestamp of when this price was computed.
+    function getPrevPriceUnsafe(bytes32 id) external returns (PythStructs.Price memory price, uint64 publishTime);
 
 }
