8e90334aa0fb6e80641d37cd19267e0304b6aecc
Tom Pointon <tom@teepeestudios.net>
2022-04-12 09:16:14 +0000

    Remove unnecessary dependency on solidity-bytes-utils

--- PythStructs.sol
+++ PythStructs.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.0;
 
-import "solidity-bytes-utils/contracts/BytesLib.sol";
-
 contract PythStructs {
 
     // A price with a degree of uncertainty, represented as a price +- a confidence interval.
