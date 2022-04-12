749ac623247ea2ee687a19106d5d9864ec1c4335
Tom Pointon <tom@teepeestudios.net>
2022-04-12 14:12:24 +0000

    Change price getters to be views

--- IPyth.sol
+++ IPyth.sol
@@ -12,18 +12,18 @@ interface IPyth {
     /// @dev Reverts if the current price is not available.
     /// @param id The Pyth Price Feed ID of which to fetch the current price and confidence interval.
     /// @return price - please read the documentation of PythStructs.Price to understand how to use this safely.
-    function getCurrentPrice(bytes32 id) external returns (PythStructs.Price memory price);
+    function getCurrentPrice(bytes32 id) external view returns (PythStructs.Price memory price);
 
     /// @notice Returns the exponential moving average price and confidence interval.
     /// @dev Reverts if the current exponential moving average price is not available.
     /// @param id The Pyth Price Feed ID of which to fetch the current price and confidence interval.
     /// @return price - please read the documentation of PythStructs.Price to understand how to use this safely.
-    function getEmaPrice(bytes32 id) external returns (PythStructs.Price memory price);
+    function getEmaPrice(bytes32 id) external view returns (PythStructs.Price memory price);
 
     /// @notice Returns the most recent previous price with a status of Trading, with the time when this was published.
     /// @dev This may be a price from arbitrarily far in the past: it is important that you check the publish time before using the price.
     /// @return price - please read the documentation of PythStructs.Price to understand how to use this safely.
     /// @return publishTime - the UNIX timestamp of when this price was computed.
-    function getPrevPriceUnsafe(bytes32 id) external returns (PythStructs.Price memory price, uint64 publishTime);
+    function getPrevPriceUnsafe(bytes32 id) external view returns (PythStructs.Price memory price, uint64 publishTime);
 
 }
