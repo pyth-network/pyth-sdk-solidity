004d13f4247e11663f289eff61aeffb7ece52bf0
Ali Behjati <bahjatia@gmail.com>
2022-06-23 20:41:51 +0200

    Abehjati/add-mock-contract-and-release (#11)
    * Add mock contract
    
    * Bump version from 0.1.0 to 0.2.0

---
+++ MockPyth.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.0;
+
+import "./AbstractPyth.sol";
+import "./PythStructs.sol";
+
+contract MockPyth is AbstractPyth {
+    mapping(bytes32 => PythStructs.PriceFeed) priceFeeds;
+
+    function queryPriceFeed(bytes32 id) public override view returns (PythStructs.PriceFeed memory priceFeed) {
+        return priceFeeds[id];
+    }
+
+    // Takes an array of encoded price feeds and stores them.
+    // You can create this data either by calling createPriceFeedData or 
+    // by using web3.js or ethers abi utilities.
+    function updatePriceFeeds(bytes[] memory updateData) public override {
+        for(uint i = 0; i < updateData.length; i++) {
+            PythStructs.PriceFeed memory priceFeed = abi.decode(updateData[i], (PythStructs.PriceFeed));
+            priceFeeds[priceFeed.id] = priceFeed;
+            emit PriceUpdate(priceFeed.id, priceFeed.publishTime);
+        }
+    }
+
+    function createPriceFeedUpdateData(
+        bytes32 id,
+        int64 price,
+        uint64 conf,
+        int32 expo,
+        uint8 status,
+        int64 emaPrice,
+        uint64 emaConf,
+        uint64 publishTime,
+        int64 prevPrice,
+        uint64 prevConf,
+        uint64 prevPublishTime
+    ) public pure returns (bytes memory priceFeedData) {
+        PythStructs.PriceFeed memory priceFeed;
+
+        priceFeed.id = id;
+        priceFeed.productId = id;
+        priceFeed.price = price;
+        priceFeed.conf = conf;
+        priceFeed.expo = expo;
+        priceFeed.status = PythStructs.PriceStatus(status);
+        priceFeed.maxNumPublishers = 10;
+        priceFeed.numPublishers = 10;
+        priceFeed.emaPrice = emaPrice;
+        priceFeed.emaConf = emaConf;
+        priceFeed.publishTime = publishTime;
+        priceFeed.prevPrice = prevPrice;
+        priceFeed.prevConf = prevConf;
+        priceFeed.prevPublishTime = prevPublishTime;
+
+        priceFeedData = abi.encode(priceFeed);
+    }
+}
